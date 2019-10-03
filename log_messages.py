ad7b83b82a065ec34674e95b69f201c884ad8d44
Kenneth Baugh <30825293+kenjbaugh@users.noreply.github.com>
2019-10-03 09:35:32 -0700

    Modified file path

--- log_messages.py
+++ log_messages.py
@@ -8,7 +8,7 @@ os.chdir("/var/log")
 
 
 def messages():
-    with open('/home/ken/myscripts/python-scripts/systemd_filtered.txt', 'w') as results:
+    with open('systemd_filtered.txt', 'w') as results:
         log_messages = subprocess.run("ls -al | cat messages | grep systemd", stdout=results, shell=True)
     return log_messages
 
